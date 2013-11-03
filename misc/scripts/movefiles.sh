473899d49f92475dc6d823fe5f21e87e5dd48ec8
brundin <brundin@ualberta.ca>
2013-11-03 12:45:46 -0700

    Shell script to move files [MRB]

---
+++ misc/scripts/movefiles.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/sh
+
+# MRB -- Fri 01-Nov-2013
+
+# Purpose: Shell script to move Orlando entity files
+
+# Description: Shell script to move Orlando entity files into their own directory.  The Orlando
+# entity files are first identified using grep, and then the file names as standard output are
+# piped as standard input to xargs.  Using the arguments from xargs, the mv command is used to
+# move the Orlando entity files from the "data" directory to the "orlando" directory, a new
+# sibling directory created at the same directory tree level as the "data" directory.  The shell
+# script is run from the parent directory of the "data" directory.  To run the script,
+# type the following at the command prompt:
+
+#     sh movefiles.sh
+
+mkdir orlando
+grep -l "<projectId>orlando</projectId>" ./data/* | xargs mv -t ./orlando
