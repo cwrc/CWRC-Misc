ce8e2215163b133948cc89080e13c2fe3664fb65
Jeffery Antoniuk <jefferya@cwrc-dev-01.srv.ualberta.ca>
2013-11-22 15:14:11 -0700

    increase efficency/speed of purge

--- migration/scripts/fedora-purge-by-list.sh
+++ migration/scripts/fedora-purge-by-list.sh
@@ -9,10 +9,10 @@ base_bin=/usr/local/fedora/client/bin
 export FEDORA_HOME=/usr/local/fedora
 
 
-echo enter PID list file name
+echo enter PID list file name E.G. "file:///tmp/lst"
 read fedora_pid_list_fn 
 
-echo enter TARGET repository E.G "localhost:8080"
+echo enter TARGET repository E.G. "localhost:8080"
 read fedora_target_repo_name
 
 
@@ -32,10 +32,13 @@ if [[ $a != "Y" && $a != "y" ]]; then
 fi
 
 
-while read line
-do
-    echo -e "$line"
-    #echo "$base_bin/fedora-purge.sh r $fedora_target_repo_name $fedora_target_repo_user $fedora_target_repo_password $line "
-    tmp="$base_bin/fedora-purge.sh $fedora_target_repo_name $fedora_target_repo_user $fedora_target_repo_password $line http \"migration purge\" "
-     $tmp
-done < $fedora_pid_list_fn 
+tmp="$base_bin/fedora-purge.sh $fedora_target_repo_name $fedora_target_repo_user $fedora_target_repo_password $fedora_pid_list_fn http \"migration purge\" "
+$tmp
+
+#while read line
+#do
+#    echo -e "$line"
+#    #echo "$base_bin/fedora-purge.sh r $fedora_target_repo_name $fedora_target_repo_user $fedora_target_repo_password $line "
+#    tmp="$base_bin/fedora-purge.sh $fedora_target_repo_name $fedora_target_repo_user $fedora_target_repo_password $line http \"migration purge\" "
+#     $tmp
+#done < $fedora_pid_list_fn 
