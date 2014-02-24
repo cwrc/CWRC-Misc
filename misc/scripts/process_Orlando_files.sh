8de39514d5b5d3dfee790ef1ed5bf13a3a293b3d
brundin <brundin@ualberta.ca>
2014-02-23 22:31:22 -0700

    New script to process Orlando files [MRB]

---
+++ misc/scripts/process_Orlando_files.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# MRB -- Sun 23-Feb-2014
+
+# Purpose: Shell script to normalize, join, and format and indent Orlando files
+
+# Description: Shell script to strip leading and trailing blank spaces (normalize), linearize
+# the document into one line, with no space between the joined lines (join), and pretty print
+# the document (format and indent).  The Orlando files to be batch processed are put in a
+# directory called "old", and the processed files are written with the same name to a directory
+# called "new"; the directory "new" is created before the script is run, and it is created at
+# the same level as the directory "old", and the shell script "process_Orlando_files.sh" is
+# placed in the parent directory of the "old" and "new" directories.  To run the script, type
+# the following at the command prompt:
+
+#     sh process_Orlando_files.sh
+
+### Begin script
+# count the number of files in the "old" directory and put this total in the TOTAL_FILES variable
+TOTAL_FILES=`ls -1 ./old | wc -l`
+# initialize the FILE_COUNT variable
+FILE_COUNT=0
+# print out an initial batch file processing statement
+echo "### Begin the batch file processing of $TOTAL_FILES Orlando documents."
+
+# loop through the "old" directory; put the file path name in the PATH_NAME variable
+for PATH_NAME in ./old/*; do
+    # increment the FILE_COUNT number by 1
+    FILE_COUNT=$[$FILE_COUNT+1]
+    # get the file name from the PATH_NAME variable, and put the file name in the FILE_NAME variable
+    FILE_NAME=`basename $PATH_NAME`
+    # print out a file processing statement
+    echo "    Processing file number $FILE_COUNT of $TOTAL_FILES files, the file $FILE_NAME . . ."
+    # normalize and join the file, and put the file contents in the TMP_VAR variable
+    TMP_VAR=`sed 's/^[ \t]*//;s/[ \t]*$//' $PATH_NAME | tr -d '\n'`
+    # format and indent the file, and write the processed file to the "new" directory
+    echo "$TMP_VAR" | XMLLINT_INDENT='    ' xmllint --format --encode utf-8 - > ./new/$FILE_NAME
+done
+
+# print out a final batch file processing statement
+echo "### The batch file processing of $TOTAL_FILES Orlando documents is now finished."
+### End script
