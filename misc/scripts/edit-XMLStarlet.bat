63dee5547e3b495058ff1932028ef55d5635ad62
brundin <brundin@ualberta.ca>
2014-09-18 14:50:35 -0600

    New edit-XMLStarlet.bat

--- misc/scripts/edit-XMLStarlet.bat
+++ misc/scripts/edit-XMLStarlet.bat
@@ -1,4 +1,4 @@
-@echo off
+	@echo off
 setlocal
 
 rem MRB -- Fri 29-Aug-2014
@@ -48,14 +48,34 @@ xml ed -L -u "/CWRC/ENTRY/FAMILY[P='Commonlaw Relationship: 2']/P" -v "Commonlaw
 rem Optional additional play: 13*11; if "Y" then change to "Yes", else delete RESEARCHNOTE and its immediate sibling TEXTSCOPE
 xml ed -L -u "/CWRC/ENTRY[RESEARCHNOTE/text()='Optional additional play: Y']/RESEARCHNOTE" -v "Optional additional play: Yes" %%f
 xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE[preceding-sibling::RESEARCHNOTE[1]/text()='Optional additional play: ']" %%f
-xml ed -L -d "/CWRC/ENTRY[RESEARCHNOTE/text()='Optional additional play: ']" %%f
+xml ed -L -d "/CWRC/ENTRY/RESEARCHNOTE[self::RESEARCHNOTE/text()='Optional additional play: ']" %%f
 rem Regional playwright: 2d; if "Y" then change to "Yes", else delete ancestor PRODUCTION (Note: "Y " has a space after it, hence use of starts-with predicate)
 xml ed -L -u "/CWRC/ENTRY/PRODUCTION/P[starts-with(PLITERARYMOVEMENTS,'Identify as a regional playwright: Y')]/PLITERARYMOVEMENTS" -v "Identify as a regional playwright: Yes" %%f
 xml ed -L -d "/CWRC/ENTRY/PRODUCTION[P/PLITERARYMOVEMENTS='Identify as a regional playwright:  ']" %%f
 rem Complete or graduate from the program: 4*1a; if "Y" then change to "Completed", else change to "Incomplete"
-xml ed -L -a "/CWRC/ENTRY/EDUCATION/CHRONSTRUCT[CHRONPROSE[descendant::RESEARCHNOTE[3][contains(.,'Did you complete or graduate from the program')]] and child::CHRONPROSE/text()[3]='Y.']/CHRONPROSE/text()[3]" -t text -n "text()" -v "Completed." %%f
+xml ed -L -a "/CWRC/ENTRY/EDUCATION/CHRONSTRUCT[CHRONPROSE[descendant::RESEARCHNOTE[3]/text()[contains(.,'Did you complete or graduate from the program')]] and child::CHRONPROSE/text()[3]='Y.']/CHRONPROSE/text()[3]" -t text -n "text()" -v "Completed." %%f
 xml ed -L -u "/CWRC/ENTRY/EDUCATION/CHRONSTRUCT[CHRONPROSE/text()[3]='Y.Completed.']/CHRONPROSE" -v "Completed." %%f
-xml ed -L -a "/CWRC/ENTRY/EDUCATION/CHRONSTRUCT[CHRONPROSE[descendant::RESEARCHNOTE[3][contains(.,'Did you complete or graduate from the program')]] and child::CHRONPROSE/text()[3]='.']/CHRONPROSE/text()[3]" -t text -n "text()" -v "Incomplete." %%f
+xml ed -L -a "/CWRC/ENTRY/EDUCATION/CHRONSTRUCT[CHRONPROSE[descendant::RESEARCHNOTE[3]/text()[contains(.,'Did you complete or graduate from the program')]] and child::CHRONPROSE/text()[3]='.']/CHRONPROSE/text()[3]" -t text -n "text()" -v "Incomplete." %%f
 xml ed -L -u "/CWRC/ENTRY/EDUCATION/CHRONSTRUCT[CHRONPROSE/text()[3]='.Incomplete.']/CHRONPROSE" -v "Incomplete." %%f
+rem Does this play have any specific casting requirements: 13b6, 13bb6_*, 13*51, 13*6_*
+rem xml ed -L -a "/CWRC/ENTRY/TEXTSCOPE[1]/TEXTUALFEATURES[P[2]/text()[15][contains(.,'Does this play have any specific casting requirements: ')]]/P[2]/text()[15]" -t text -n "text()" -v "MRB" %%f
+rem /CWRC/ENTRY[1]/TEXTSCOPE[1]/TEXTUALFEATURES[1]/P[2]/RESEARCHNOTE[16]
+rem xml ed -L -u "/CWRC/ENTRY/TEXTSCOPE[1]/TEXTUALFEATURES[P[2]/text()[15][contains(.,'Does this play have any specific casting requirements: ')]]/P[2]/text()[15]" -v "MRB" %%f
+xml ed -L -u "/CWRC/ENTRY/TEXTSCOPE[1]/TEXTUALFEATURES[P[2]/text()[15]='Does this play have any specific casting requirements: Y']/P[2]/text()[15]" -v "Does this play have any specific casting requirements: Yes" %%f
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE[1]/TEXTUALFEATURES/P[2]/RESEARCHNOTE[17][parent::P/text()[15]='Does this play have any specific casting requirements: ']" %%f
+rem xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE[1]/TEXTUALFEATURES/P[2]/text()[16][preceding-sibling::text()[15]='Does this play have any specific casting requirements: ']" %%f
 
+
+rem /*/bar[position() >= 100 and not(position() > 200)]
+rem //foo/bar[100 <= position() and position() < 200]
+
+rem Age, Gender
+
+rem Actors who are auditioning
+rem Young actors
+rem Published in amy format
+rem Previous partner; remove if empty (full paragraph P); also remove preceding sibling research note
+rem Religious affiliation; if empty, insert "None given"
+rem Historical period: remove period at end of list of items
+rem Genre: remove period at end of list of items
 )
