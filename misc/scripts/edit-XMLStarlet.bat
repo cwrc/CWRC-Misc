dd2d440e61f02915616540748a3d7887f3f8e968
brundin <brundin@ualberta.ca>
2014-09-19 15:08:44 -0600

    New edit-XMLStarlet.bat file [MRB]

--- misc/scripts/edit-XMLStarlet.bat
+++ misc/scripts/edit-XMLStarlet.bat
@@ -20,7 +20,7 @@ rem Note: XMLStarlet must be installed; the XMLStarlet executable is called via 
 :: loop through the "files" directory
 for /r files %%f in (*) do (
 
-:: XML element text node value substitutions
+:: XML text node value substitutions
 rem Gender: 1b1
 xml ed -L -u "/CWRC/ENTRY/CULTURALFORMATION/P/SEXUALIDENTITY[GENDER='2']/GENDER" -v "Female" %%f
 xml ed -L -u "/CWRC/ENTRY/CULTURALFORMATION/P/SEXUALIDENTITY[GENDER='3']/GENDER" -v "Male" %%f
@@ -44,7 +44,7 @@ rem Common-law relationship: 3a2
 xml ed -L -u "/CWRC/ENTRY/FAMILY[P='Commonlaw Relationship: 1']/P" -v "Commonlaw Relationship: Different sex partner" %%f
 xml ed -L -u "/CWRC/ENTRY/FAMILY[P='Commonlaw Relationship: 2']/P" -v "Commonlaw Relationship: Same sex partner" %%f
 
-:: XML element text node value substitutions, as well as XML node deletions
+:: XML text node value substitutions, as well as XML element node deletions
 rem Optional additional play: 13*11; if "Y" then change to "Yes", else delete RESEARCHNOTE and its immediate sibling TEXTSCOPE
 xml ed -L -u "/CWRC/ENTRY[RESEARCHNOTE/text()='Optional additional play: Y']/RESEARCHNOTE" -v "Optional additional play: Yes" %%f
 xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE[preceding-sibling::RESEARCHNOTE[1]/text()='Optional additional play: ']" %%f
@@ -57,24 +57,44 @@ xml ed -L -a "/CWRC/ENTRY/EDUCATION/CHRONSTRUCT[CHRONPROSE[descendant::RESEARCHN
 xml ed -L -u "/CWRC/ENTRY/EDUCATION/CHRONSTRUCT[CHRONPROSE/text()[3]='Y.Completed.']/CHRONPROSE" -v "Completed." %%f
 xml ed -L -a "/CWRC/ENTRY/EDUCATION/CHRONSTRUCT[CHRONPROSE[descendant::RESEARCHNOTE[3]/text()[contains(.,'Did you complete or graduate from the program')]] and child::CHRONPROSE/text()[3]='.']/CHRONPROSE/text()[3]" -t text -n "text()" -v "Incomplete." %%f
 xml ed -L -u "/CWRC/ENTRY/EDUCATION/CHRONSTRUCT[CHRONPROSE/text()[3]='.Incomplete.']/CHRONPROSE" -v "Incomplete." %%f
-rem Does this play have any specific casting requirements: 13b6, 13bb6_*, 13*51, 13*6_*
-rem xml ed -L -a "/CWRC/ENTRY/TEXTSCOPE[1]/TEXTUALFEATURES[P[2]/text()[15][contains(.,'Does this play have any specific casting requirements: ')]]/P[2]/text()[15]" -t text -n "text()" -v "MRB" %%f
-rem /CWRC/ENTRY[1]/TEXTSCOPE[1]/TEXTUALFEATURES[1]/P[2]/RESEARCHNOTE[16]
-rem xml ed -L -u "/CWRC/ENTRY/TEXTSCOPE[1]/TEXTUALFEATURES[P[2]/text()[15][contains(.,'Does this play have any specific casting requirements: ')]]/P[2]/text()[15]" -v "MRB" %%f
-xml ed -L -u "/CWRC/ENTRY/TEXTSCOPE[1]/TEXTUALFEATURES[P[2]/text()[15]='Does this play have any specific casting requirements: Y']/P[2]/text()[15]" -v "Does this play have any specific casting requirements: Yes" %%f
-xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE[1]/TEXTUALFEATURES/P[2]/RESEARCHNOTE[17][parent::P/text()[15]='Does this play have any specific casting requirements: ']" %%f
-rem xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE[1]/TEXTUALFEATURES/P[2]/text()[16][preceding-sibling::text()[15]='Does this play have any specific casting requirements: ']" %%f
+rem Suitable for young actors: 13*8; if "Y" then change to "Yes", else delete text node and its immediate sibling element node RESEARCHNOTE
+xml ed -L -u "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES[P[2]/text()[32]='Does this play include monologues that are suitable for young actors: Y']/P[2]/text()[32]" -v "Does this play include monologues that are suitable for young actors: Yes" %%f
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/text()[32][parent::P/text()[32]='Does this play include monologues that are suitable for young actors: '] | /CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/RESEARCHNOTE[33][parent::P/text()[32]='Does this play include monologues that are suitable for young actors: ']" %%f
+rem Actors who are auditioning: 13*7; if "Y" then change to "Yes", else delete text node and its immediate sibling element node RESEARCHNOTE
+xml ed -L -u "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES[P[2]/text()[31]='Does this play include monologues that are suitable for actors who are auditioning: Y']/P[2]/text()[31]" -v "Does this play include monologues that are suitable for actors who are auditioning: Yes" %%f
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/text()[31][parent::P/text()[31]='Does this play include monologues that are suitable for actors who are auditioning:  '] | /CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/RESEARCHNOTE[32][parent::P/text()[31]='Does this play include monologues that are suitable for actors who are auditioning:  ']" %%f
+rem Does this play have any specific casting requirements: 13*6_*; if no value, then delete text node and its immediate sibling element node RESEARCHNOTE
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/text()[30][parent::P/text()[30]='Requirement:  '] | /CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/RESEARCHNOTE[31][parent::P/text()[30]='Requirement:  ']" %%f
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/text()[29][parent::P/text()[29]='Gender:  '] | /CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/RESEARCHNOTE[30][parent::P/text()[29]='Gender:  ']" %%f
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/text()[28][parent::P/text()[28]='Age:  '] | /CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/RESEARCHNOTE[29][parent::P/text()[28]='Age:  ']" %%f
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/text()[27][parent::P/text()[27]='Requirement:  '] | /CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/RESEARCHNOTE[28][parent::P/text()[27]='Requirement:  ']" %%f
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/text()[26][parent::P/text()[26]='Gender:  '] | /CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/RESEARCHNOTE[27][parent::P/text()[26]='Gender:  ']" %%f
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/text()[25][parent::P/text()[25]='Age:  '] | /CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/RESEARCHNOTE[26][parent::P/text()[25]='Age:  ']" %%f
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/text()[24][parent::P/text()[24]='Requirement:  '] | /CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/RESEARCHNOTE[25][parent::P/text()[24]='Requirement:  ']" %%f
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/text()[23][parent::P/text()[23]='Gender:  '] | /CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/RESEARCHNOTE[24][parent::P/text()[23]='Gender:  ']" %%f
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/text()[22][parent::P/text()[22]='Age:  '] | /CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/RESEARCHNOTE[23][parent::P/text()[22]='Age:  ']" %%f
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/text()[21][parent::P/text()[21]='Requirement:  '] | /CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/RESEARCHNOTE[22][parent::P/text()[21]='Requirement:  ']" %%f
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/text()[20][parent::P/text()[20]='Gender:  '] | /CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/RESEARCHNOTE[21][parent::P/text()[20]='Gender:  ']" %%f
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/text()[19][parent::P/text()[19]='Age:  '] | /CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/RESEARCHNOTE[20][parent::P/text()[19]='Age:  ']" %%f
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/text()[18][parent::P/text()[18]='Requirement:  '] | /CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/RESEARCHNOTE[19][parent::P/text()[18]='Requirement:  ']" %%f
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/text()[17][parent::P/text()[17]='Gender:  '] | /CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/RESEARCHNOTE[18][parent::P/text()[17]='Gender:  ']" %%f
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/text()[16][parent::P/text()[16]='Age:  '] | /CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/RESEARCHNOTE[17][parent::P/text()[16]='Age:  ']" %%f
+rem Does this play have any specific casting requirements: 13b6, 13*51; if "Y" then change to "Yes", else delete text node and its immediate sibling element node RESEARCHNOTE
+xml ed -L -u "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES[P[2]/text()[15]='Does this play have any specific casting requirements: Y']/P[2]/text()[15]" -v "Does this play have any specific casting requirements: Yes" %%f
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/text()[15][parent::P/text()[15]='Does this play have any specific casting requirements: '] | /CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/RESEARCHNOTE[16][parent::P/text()[15]='Does this play have any specific casting requirements: ']" %%f
+rem General description of performers in play: 13*5_[*_SQ00*]
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/text()[14][parent::P/text()[14]='Number of Female Seniors:  '] | /CWRC/ENTRY/TEXTSCOPE/TEXTUALFEATURES/P[2]/RESEARCHNOTE[15][parent::P/text()[14]='Number of Female Seniors:  ']" %%f
 
 
-rem /*/bar[position() >= 100 and not(position() > 200)]
-rem //foo/bar[100 <= position() and position() < 200]
 
-rem Age, Gender
+rem Published in any format: 13*9; if "Y" then change to "Yes", else delete parent PRODUCTION
+xml ed -L -u "/CWRC/ENTRY/TEXTSCOPE/PRODUCTION[RESEARCHNOTE[1]/text()='Has the play been published for distribution to the public in any format: Y']/RESEARCHNOTE[1]/text()" -v "Has the play been published for distribution to the public in any format: Yes" %%f
+xml ed -L -d "/CWRC/ENTRY/TEXTSCOPE/PRODUCTION[RESEARCHNOTE[1]='Has the play been published for distribution to the public in any format: ']" %%f
+rem Previous Partner(s): 3c; if no value, then delete text node and its immediate sibling element node RESEARCHNOTE
 
-rem Actors who are auditioning
-rem Young actors
-rem Published in amy format
-rem Previous partner; remove if empty (full paragraph P); also remove preceding sibling research note
+
+
+
 rem Religious affiliation; if empty, insert "None given"
 rem Historical period: remove period at end of list of items
 rem Genre: remove period at end of list of items
